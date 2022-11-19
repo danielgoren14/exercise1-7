12d64480080ed4ac093fc3089a0c42790f5879a1
danielgoren14 <117838891+danielgoren14@users.noreply.github.com>
2022-11-19 22:29:30 +0200

    Update exercise7.java

--- src/exercise7.java
+++ src/exercise7.java
@@ -21,10 +21,10 @@ public class exercise7
             secondDigit = ((checkedNumber % 10) * (checkedNumber % 10) * (checkedNumber % 10));
             checkedNumber /= 10;
             if(i >= 100 && i <=999){
-                thirdDigit = ((checkedNumber % 10) * (checkedNumber % 10) * (checkedNumber % 10)); 
-                checkedNumber /= 10;
+                thirdDigit = ((checkedNumber % 10) * (checkedNumber % 10) * (checkedNumber % 10));
             }
             if(i == 1000) {
+                    checkedNumber /= 10;
                     fourthDigit = ((checkedNumber % 10) * (checkedNumber % 10) * (checkedNumber % 10) * (checkedNumber % 10));
                 }
             sum = firstDigit + secondDigit + thirdDigit + fourthDigit;
